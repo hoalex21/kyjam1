cfc6b6dbc8bb8e19e3c415aab31d53dfee5b8b1f
Alex Ho <alexho@Alexs-Air.fios-router.home>
2024-01-13 11:14:45 -0500

    add purple guy animation

--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 50.0
+const SPEED = 47.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 
--- purple_guy.gd
+++ purple_guy.gd
@@ -11,6 +11,7 @@ var player
 func _ready():
 	navigation_agent = $NavigationAgent2D
 	player = get_parent().get_node("Player")
+	$AnimatedSprite2D.play("walk")
 
 
 func _process(delta):
@@ -20,6 +21,11 @@ func _process(delta):
 		navigation_agent.target_position = player.position
 		direction = navigation_agent.get_next_path_position() - global_position
 		direction = direction.normalized()
+	
+	if direction.x < 0:
+		$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.flip_h = false
 	
 	velocity = direction * SPEED
 	move_and_slide()
