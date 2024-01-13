95f0021c8888104a81bb6e393729fee85af4bf6a
Alex Ho <alexho@Alexs-Air.fios-router.home>
2024-01-12 21:47:02 -0500

    add ladder

--- main.gd
+++ main.gd
@@ -28,6 +28,10 @@ const ROOMS = {
 	}
 }
 
+const PURPLE_GUY = preload("res://purple_guy.tscn")
+
+const LADDER = preload("res://ladder.tscn")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -41,22 +45,24 @@ func _process(delta):
 
 func generate_rooms():
 	var dungeon = [
-		[false, false, false, false, false, false, false, false, false, false],
-		[false, false, false, false, false, false, false, false, false, false],
-		[false, false, false, false, false, false, false, false, false, false],
-		[false, false, false, false, false, false, false, false, false, false],
-		[false, false, false, false, true, false, false, false, false, false],
-		[false, false, false, false, false, false, false, false, false, false],
-		[false, false, false, false, false, false, false, false, false, false],
-		[false, false, false, false, false, false, false, false, false, false],
-		[false, false, false, false, false, false, false, false, false, false],
-		[false, false, false, false, false, false, false, false, false, false],
+		[null, null, null, null, null, null, null, null, null, null],
+		[null, null, null, null, null, null, null, null, null, null],
+		[null, null, null, null, null, null, null, null, null, null],
+		[null, null, null, null, null, null, null, null, null, null],
+		[null, null, null, null, true, null, null, null, null, null],
+		[null, null, null, null, null, null, null, null, null, null],
+		[null, null, null, null, null, null, null, null, null, null],
+		[null, null, null, null, null, null, null, null, null, null],
+		[null, null, null, null, null, null, null, null, null, null],
+		[null, null, null, null, null, null, null, null, null, null],
 	]
 	
 	var i = 0
+	var ladder = 1
+	
+	var num: int
 	
 	while i < 30:
-		var num: int
 		for j in dungeon.size():
 			for k in dungeon[j].size():
 				if dungeon[j][k]:
@@ -65,11 +71,19 @@ func generate_rooms():
 						if num < 4:
 							dungeon[j-1][k] = true
 							i += 1
+						elif ladder == 1 and j not in [3, 4, 5] and k not in [3, 4, 5]:
+							dungeon[j-1][k] = "ladder"
+							ladder -= 1
+							i += 1
 					
 					if j < 9:
 						num = randi_range(0, 5)
 						if num < 4:
 							dungeon[j+1][k] = true
+							i += 1
+						elif ladder == 1 and j not in [3, 4, 5] and k not in [3, 4, 5]:
+							dungeon[j-1][k] = "ladder"
+							ladder -= 1
 							i += 1
 					
 					if k > 0:
@@ -77,16 +91,25 @@ func generate_rooms():
 						if num < 4:
 							dungeon[j][k-1] = true
 							i += 1
+						elif ladder == 1 and j not in [3, 4, 5] and k not in [3, 4, 5]:
+							dungeon[j-1][k] = "ladder"
+							ladder -= 1
+							i += 1
 					
 					if k < 9:
 						num = randi_range(0, 5)
 						if num < 4:
 							dungeon[j][k+1] = true
 							i += 1
+						elif ladder == 1 and j not in [3, 4, 5] and k not in [3, 4, 5]:
+							dungeon[j-1][k] = "ladder"
+							ladder -= 1
+							i += 1
 
 	for j in dungeon[0].size():
 		for k in dungeon[j].size():
-			if dungeon[j][k]:
+			var current_room = dungeon[j][k]
+			if current_room:
 				var doors = 0
 				var left = false
 				var down = false
@@ -115,46 +138,59 @@ func generate_rooms():
 				if doors == 1:
 					rooms = ROOMS.one_door
 					if left:
-						instantiate_room(rooms.right, j, k)
+						instantiate_room(rooms.right, j, k, current_room)
 					elif down:
-						instantiate_room(rooms.up, j, k)
+						instantiate_room(rooms.up, j, k, current_room)
 					elif right:
-						instantiate_room(rooms.left, j, k)
+						instantiate_room(rooms.left, j, k, current_room)
 					elif up:
-						instantiate_room(rooms.down, j, k)
+						instantiate_room(rooms.down, j, k, current_room)
 				elif doors == 2:
 					rooms = ROOMS.two_door
 					if left && down:
-						instantiate_room(rooms.right_up, j, k)
+						instantiate_room(rooms.right_up, j, k, current_room)
 					if left && right:
-						instantiate_room(rooms.left_right, j, k)
+						instantiate_room(rooms.left_right, j, k, current_room)
 					if left && up:
-						instantiate_room(rooms.down_right, j, k)
+						instantiate_room(rooms.down_right, j, k, current_room)
 					if down && right:
-						instantiate_room(rooms.left_up, j, k)
+						instantiate_room(rooms.left_up, j, k, current_room)
 					if down && up:
-						instantiate_room(rooms.down_up, j, k)
+						instantiate_room(rooms.down_up, j, k, current_room)
 					if right && up:
-						instantiate_room(rooms.left_down, j, k)
+						instantiate_room(rooms.left_down, j, k, current_room)
 				elif doors == 3:
 					rooms = ROOMS.three_door
 					if left && down && right:
-						instantiate_room(rooms.left_right_up, j, k)
+						instantiate_room(rooms.left_right_up, j, k, current_room)
 					if left && down && up:
-						instantiate_room(rooms.down_right_up, j, k)
+						instantiate_room(rooms.down_right_up, j, k, current_room)
 					if left && right && up:
-						instantiate_room(rooms.left_down_right, j, k)
+						instantiate_room(rooms.left_down_right, j, k, current_room)
 					if down && right && up:
-						instantiate_room(rooms.left_down_up, j, k)
+						instantiate_room(rooms.left_down_up, j, k, current_room)
 				elif doors == 4:
 					rooms = ROOMS.four_door
-					instantiate_room(rooms.left_down_right_up, j, k)
+					instantiate_room(rooms.left_down_right_up, j, k, current_room)
 
 
-func instantiate_room(ref, j, k):
+func instantiate_room(ref, j, k, current_room_value):
 	j -= 4
 	k -= 4
-	var instance = ref.instantiate()
-	instance.position = Vector2(-448 * j, -448 * k)
-	add_child(instance)
-	move_child(instance, 0)
+	var room = ref.instantiate()
+	room.position = Vector2(-448 * j, -448 * k)
+	add_child(room)
+	move_child(room, 0)
+	
+	if typeof(current_room_value) == TYPE_STRING:
+		if current_room_value == "ladder":
+			var ladder = LADDER.instantiate()
+			ladder.position = room.position
+			add_child(ladder)
+			move_child(ladder, $Player.get_index())
+	elif j not in [-1, 0, 1] and k not in [-1, 0, 1]:
+		var num = randi_range(0, 1)
+		if num == 0:
+			var purple_guy = PURPLE_GUY.instantiate()
+			purple_guy.position = room.position
+			add_child(purple_guy)
